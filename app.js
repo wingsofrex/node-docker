ef552cdfbac35d58768759a65eea95dbcb606b7a
Rainak Sharma <rainak2k@gmail.com>
2026-08-29 00:33:23 +0530

    Add Jenkins webhook test comment to app.js
    Added a comment for Jenkins webhook test.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World from Node.js in Docker!\n');
 });
-
+// Jenkins webhook test
 // Start the server
 server.listen(PORT, () => {
     console.log(`Server running at http://0.0.0.0:${PORT}/`);
